5639594c34ac5d419bab6704aab0c7db441e0534
Sidónio Goenha <sid.goenha@gmail.com>
2020-06-08 09:17:41 +0200

    Projecto Cadastro de Usuários (React)

---
+++ react/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+       Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>COD<span className="text-danger">3</span>R</strong>
+    </footer>
